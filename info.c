9bd10abb99f4d78d62ebd4b4399367f81f7daef4
yonige2 <31758217+yonige2@users.noreply.github.com>
2017-09-08 15:22:26 +0900

    My information
    This is my information.

---
+++ info.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("==============================================\n");
+	printf("My student id number : 2017301069\n");
+	printf("==============================================\n");
+	printf("My name : Jeong Jong-Duck\n");
+	printf("==============================================\n");
+	printf("My mobile phone number : 010-5471-7991\n");
+	printf("==============================================\n");
+	printf("My GitHub address : https://github.com/yonige2\n");
+	printf("==============================================\n");
+	
+	return 0;
+}
